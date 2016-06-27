3f157f770106b0cb5d4631ec5a8b40028fbad0ef
Ellmi <ymxing@thoughtworks.com>
2016-06-27 23:53:47 +0800

    [Wedding-11] Yanmin: fixed the manu part.

--- template_inc/inc-menu.php
+++ template_inc/inc-menu.php
@@ -19,32 +19,56 @@
                 ?>
             </a>
         </h3>
-        <button data-target=".nav-collapse" class="btn-navbar icon_menu" type="button">
-            <i class="fa fa-bars"></i>
-        </button>
-        <button class="search pull-right"> <i class="fa fa-search"></i></button>
-        <nav class="pull-right">
-            <?php
-            if ( has_nav_menu('primary') ):
-                wp_nav_menu(array(
-                    'theme_location' =>  'primary',
-                    'container'     =>  false,
-                    'menu_class'    =>  'menu nav-collapse'
-                ));
-            else:
-                ?>
-                <ul class="menu">
-                    <li>
-                        <a href="<?php echo get_admin_url().'/nav-menus.php'; ?>">
-                            ADD TO MENU
-                        </a>
-                    </li>
-                </ul>
-            <?php
-            endif;
-            ?>
+        <nav class="pull-right tznav-menu">
+            <ul id="menu-primary" class="menu nav-collapse themeple_megemenu">
+                <li id="menu-item-102"
+                    class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-has-children">
+                    <a href="wedding">婚礼策划</a>
+
+                    <ul class="sub-menu non_mega_menu">
+                        <li id="menu-item-103"
+                            class="menu-item menu-item-type-post_type menu-item-object-page"><a
+                                href="#">婚礼套餐</a>
+                        </li>
+                        <li id="menu-item-149"
+                            class="menu-item menu-item-type-post_type menu-item-object-page"><a
+                                href="#">终端定制</a>
+                        </li>
+                        <li id="menu-item-163"
+                            class="menu-item menu-item-type-post_type menu-item-object-page"><a
+                                href="#">求婚策划</a>
+                        </li>
+                        <li id="menu-item-173"
+                            class="menu-item menu-item-type-post_type menu-item-object-page"><a
+                                href="#">会议策划</a>
+                        </li>
+                        <li id="menu-item-497"
+                            class="menu-item menu-item-type-custom menu-item-object-custom"><a
+                                href="#">高端手绘设计</a></li>
+                    </ul>
+                </li>
+                <li id="menu-item-397"
+                    class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                    <a href="demo">婚礼案例</a>
+                </li>
+                <li id="menu-item-183"
+                    class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children">
+                    <a href="video">视频案例</a>
+                </li>
+                <li id="menu-item-279"
+                    class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                    <a href="hotel">酒店</a>
+                </li>
+                <li id="menu-item-373"
+                    class="menu-item menu-item-type-taxonomy menu-item-object-tribe_events_cat menu-item-has-children">
+                    <a href="activity">活动</a>
+                </li>
+                <li id="menu-item-20"
+                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children">
+                    <a href="taobao">淘宝</a>
+                </li>
+            </ul>
         </nav>
-    </div>
 </header>
 <div class="tzform-search">
     <img class="tzcontro_search" src="<?php echo esc_url(IMG_PATH.'icon_search_delete.png') ; ?>" alt="icon search">
