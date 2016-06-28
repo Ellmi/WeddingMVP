a4ad725b750f84497230c3363ac489bc312444ff
Ellmi <ymxing@thoughtworks.com>
2016-06-29 00:00:59 +0800

    [Wedding-12] Yanmin:demos page done.

--- template-demos.php
+++ template-demos.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Demos Template
+ * Template Name: Template Demos
  * User: Yanmin
  */
 get_header();
@@ -10,30 +10,38 @@ get_template_part('template_inc/slides', 'home'); ?>
         <div class="row">
             <h1 class="playlist_title">婚礼案例</h1>
             <?php
+            $paged = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1;
             $blog_args = array(
                 'post_type' => 'post',
-                'category_name' => 'demo',
-                'posts_per_page' => 2
+                'category_name' => 'demos',
+                'posts_per_page' => 6,
+                'paged' => $paged
             );
-            $query_blog = new WP_Query($blog_args);
-            if ($query_blog->have_posts()):
-                while ($query_blog->have_posts()):
-                    $query_blog->the_post(); ?>
-                    <div class="dl-demo wpb_column vc_column_container vc_col-sm-12 vc_col-lg-6 vc_col-lg-6">
-                        <div class="vc_column-inner">
-                            <?php the_content();
-                            wp_link_pages(array(
-                                'before' => '<div class="page-links"><span class="page-links-title">' . __('Pages:', TEXT_DOMAIN) . '</span>',
-                                'after' => '</div>',
-                                'link_before' => '<span>',
-                                'link_after' => '</span>',
-                            ));?>
-                        </div>
+            query_posts($blog_args);
+            if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <div class="dl-demo wpb_column vc_column_container vc_col-sm-12 vc_col-lg-6 vc_col-lg-6">
+                    <div class="vc_column-inner">
+                        <?php the_content();?>
                     </div>
-                    <?php
-                endwhile;
+                </div>
+            <?php endwhile;
+            else :
             endif;
             wp_reset_postdata();?>
+        </div>
+        <div class="navigation">
+            <?php
+            global $wp_query;
+
+            $big = 999999999; // need an unlikely integer
+
+            echo paginate_links(array(
+                'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+                'format' => '?paged=%#%',
+                'current' => max(1, get_query_var('paged')),
+                'total' => $wp_query->max_num_pages
+            ));
+            ?>
         </div>
     </div>
 
