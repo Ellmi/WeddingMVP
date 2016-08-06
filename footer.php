4a7b2fcc4d8e1e1480bd7308421e626224606c02
Ellmi <ymxing@thoughtworks.com>
2016-08-06 17:52:41 +0800

    [Wedding-25] Yanmin:add icon to footer.

--- footer.php
+++ footer.php
@@ -2,11 +2,11 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-sm-6 col-xs-12 footerattr">
-                <p class="one"><?php echo esc_attr_e(get_theme_mod('Footer_social_one', __('电话: XXXXXXXXXX', 'deeplove')));?></p>
+                <p class="one"><i class="fa fa-headphones"></i><?php echo esc_attr_e(get_theme_mod('Footer_social_one', __('电话: XXXXXXXXXX', 'deeplove')));?></p>
 
-                <p class="two"><?php echo esc_attr_e(get_theme_mod('Footer_social_two', __('QQ: XXXXXXXXXX', 'deeplove')));?></p>
+                <p class="two"><i class="fa fa-qq"></i><?php echo esc_attr_e(get_theme_mod('Footer_social_two', __('QQ: XXXXXXXXXX', 'deeplove')));?></p>
 
-                <p class="three"><?php echo esc_attr_e(get_theme_mod('Footer_social_three', __('微信: XXXXXXXXXX', 'deeplove')));?></p>
+                <p class="three"><i class="fa fa-wechat"></i><?php echo esc_attr_e(get_theme_mod('Footer_social_three', __('微信: XXXXXXXXXX', 'deeplove')));?></p>
             </div>
             <div class="col-md-4 col-sm-6 col-xs-12 footerattr">
                 <a href="#">
@@ -14,11 +14,11 @@
                 </a>
             </div>
             <div class="col-md-4 col-sm-6 col-xs-12 footerattr">
-                <p class="four"><?php echo esc_attr_e(get_theme_mod('Footer_social_four', __('地址: XXXXXXXXXXX', 'deeplove')));?></p>
+                <p class="four"><i class="fa fa-location-arrow"></i><?php echo esc_attr_e(get_theme_mod('Footer_social_four', __('地址: XXXXXXXXXXX', 'deeplove')));?></p>
 
-                <p class="five"><?php echo esc_attr_e(get_theme_mod('Footer_social_five', __('淘宝店铺: XXXXXXXXXX', 'deeplove')));?></p>
+                <a class="five" href="https://shop156590744.world.taobao.com/"><i class="fa fa-at"></i><?php echo esc_attr_e(get_theme_mod('Footer_social_five', __('淘宝店铺: XXXXXXXXXX', 'deeplove')));?></a>
 
-                <p class="six"><?php echo esc_attr_e(get_theme_mod('Footer_social_six', __('其他: XXXXXXXXXX', 'deeplove')));?></p>
+<!--                <p class="six"></i>--><?php //echo esc_attr_e(get_theme_mod('Footer_social_six', __('其他: XXXXXXXXXX', 'deeplove')));?><!--</p>-->
             </div>
 
         </div>
