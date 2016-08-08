50ede0c87d29a14197ea966268f95fdcca8733c0
Ellmi <ymxing@thoughtworks.com>
2016-08-08 22:04:07 +0800

    [Wedding-29] Yanmin:fixed some slides bug.

--- template_inc/slides-home.php
+++ template_inc/slides-home.php
@@ -6,13 +6,13 @@
 <div id="homepage-slider" style="z-index: 2" class="deeplove-slider">
     <ul class="slides">
         <li>
-            <img style="height:844px;" src="<?php echo esc_url(get_theme_mod('slide1_image', get_template_directory_uri() . '/images/slides/slider1.jpg')); ?>" />
+            <img src="<?php echo esc_url(get_theme_mod('slide1_image', get_template_directory_uri() . '/images/slides/slider1.jpg')); ?>" />
         </li>
         <li>
-            <img style="height:844px;" src="<?php echo esc_url(get_theme_mod('slide2_image', get_template_directory_uri() . '/images/slides/slider2.jpg')); ?>" />
+            <img src="<?php echo esc_url(get_theme_mod('slide2_image', get_template_directory_uri() . '/images/slides/slider2.jpg')); ?>" />
         </li>
         <li>
-            <img style="height:844px;" src="<?php echo esc_url(get_theme_mod('slide3_image', get_template_directory_uri() . '/images/slides/slider3.jpg')); ?>" />
+            <img src="<?php echo esc_url(get_theme_mod('slide3_image', get_template_directory_uri() . '/images/slides/slider3.jpg')); ?>" />
         </li>
     </ul>
 </div>
--- template_inc/slides-video.php
+++ template_inc/slides-video.php
@@ -6,16 +6,13 @@
 <div id="video-slider" class="deeplove-slider" style="z-index: 2">
     <ul class="slides">
         <li>
-            <img style="height:844px;"
-                 src="<?php echo esc_url(get_theme_mod('video_slide1_image', get_template_directory_uri() . '/images/slides/slider1.jpg')); ?>"/>
+            <img src="<?php echo esc_url(get_theme_mod('video_slide1_image', get_template_directory_uri() . '/images/slides/slider1.jpg')); ?>"/>
         </li>
         <li>
-            <img style="height:844px;"
-                 src="<?php echo esc_url(get_theme_mod('video_slide2_image', get_template_directory_uri() . '/images/slides/slider2.jpg')); ?>"/>
+            <img src="<?php echo esc_url(get_theme_mod('video_slide2_image', get_template_directory_uri() . '/images/slides/slider2.jpg')); ?>"/>
         </li>
         <li>
-            <img style="height:844px;"
-                 src="<?php echo esc_url(get_theme_mod('video_slide3_image', get_template_directory_uri() . '/images/slides/slider3.jpg')); ?>"/>
+            <img src="<?php echo esc_url(get_theme_mod('video_slide3_image', get_template_directory_uri() . '/images/slides/slider3.jpg')); ?>"/>
         </li>
     </ul>
 </div>
